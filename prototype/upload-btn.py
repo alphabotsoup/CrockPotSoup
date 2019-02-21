91385fe7e5281e9956b96c057c1c63a0bb58a151
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2019-02-20 18:58:11 -0600

    Fixed file upload demo. Decoding has been resolved and a simple example is provided which converts a given CSV file into an HTML table.

--- prototype/upload-btn.py
+++ prototype/upload-btn.py
@@ -13,27 +13,35 @@ app = dash.Dash(__name__)
 
 # Page layout
 app.layout = html.Div([
-	dcc.Upload(
-		html.Button('Upload'),
-		id='input-box'
-	),
-	html.Div(id='output-box')
+    dcc.Upload(
+        html.Button('Upload'),
+        id='input-box'
+    ),
+    html.Div(id='output-box')
 ])
 
 
 # Callbacks (functionality)
 @app.callback(
-	Output('output-box', 'children'),
-	[Input('input-box', 'contents')]
+    Output('output-box', 'children'),
+    [Input('input-box', 'contents')]
 )
 def post_file(file_contents):
-	if file_contents:
-		rows = []
-		for line in csv.reader(iter(str(b64decode(file_contents)).splitlines())):
-			rows.append(html.Tr([html.Td(col) for col in line]))
-		return html.Table(rows)
+    """
+    Basic routine to accept a file upload, decode it, and interpret it as CSV.
+    Examples at: `dash.plot.ly/dash-core-components/upload`
+    File contents come in the form of "content_type,content_string" and are Base 64 encoded.
+    base64.b64decode() returns type 'bytes', which must then be decoded into Unicode.
+    """
+    if file_contents:
+        file_contents = b64decode(file_contents.split(',')[1], validate=True).decode('utf-8')
+
+        rows = []
+        for line in csv.reader(iter(file_contents.splitlines())):
+            rows.append(html.Tr([html.Td(col) for col in line]))
+        return html.Table(rows)
 
 
 # Debug mode when this script is invoked directly
 if __name__ == '__main__':
-	app.run_server(debug=True)
+    app.run_server(debug=True)
