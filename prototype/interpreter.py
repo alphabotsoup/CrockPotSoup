18ee8892b17d7838282fee04e98f74a24a6ad36c
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2019-05-07 09:15:55 -0500

    Added intrinsics, preparing to add compilation phase

--- prototype/interpreter.py
+++ prototype/interpreter.py
@@ -3,15 +3,95 @@ from pattern.en import parse, pprint, parsetree, Chunk
 from pattern.search import search
 
 ### Init
-code = ""   # Container for pseudocode (Sprint 3 demonstration of function calls)
+pseudocode = ""   # Container for pseudocode (Sprint 3 demonstration of function calls)
 
 # Declare contextual variables (inspired by Perl speclal/default variables)
 # Nouns are basically copied here when referenced so that pronouns / implicit references can access them
 context_vars = {
-    "male": None,   # "he", "his", etc
-    "female": None, # "she", "her", etc
+#    "male": None,   # "he", "his", etc     Excluded from prototype
+#    "female": None, # "she", "her", etc    Excluded from prototype
     "var": None,    # "that", "its", etc -- also includes male *or* female references
     "array": []     # "they", "their", "those", etc -- no gendered plural pronouns as far as I'm aware
+}
+
+### Functions (intrinsic operations, "standard" functions)
+# Arithmetic
+# This section right here is a fantastic candidate for the "runtime generated code" concept I presented about in class
+def add(*args):
+    n = 0
+    for arg in args:
+        if isinstance(arg, list):
+            # If an argument is a list of numbers, unpack and make recursive call
+            n += add(*arg)
+        else:
+            n += arg
+    return n
+def sub():
+    n = args[0]
+    for arg in args[1:]:
+        if isinstance(arg, list):
+            # If an argument is a list of numbers, unpack and make recursive call
+            n -= sub(*arg)
+        else:
+            n -= arg
+    return n
+def mul():
+    n = args[0]
+    for arg in args[1:]:
+        if isinstance(arg, list):
+            # If an argument is a list of numbers, unpack and make recursive call
+            n *= mul(*arg)
+        else:
+            n *= arg
+    return n
+def div():
+    n = args[0]
+    for arg in args[1:]:
+        if isinstance(arg, list):
+            # If an argument is a list of numbers, unpack and make recursive call
+            n /= div(*arg)
+        else:
+            n /= arg
+    return n
+def mod():
+    n = args[0]
+    for arg in args[1:]:
+        if isinstance(arg, list):
+            # If an argument is a list of numbers, unpack and make recursive call
+            n %= mod(*arg)
+        else:
+            n %= arg
+    return n
+def inc(num):
+    return num+1
+def dec(num):
+    return num-1
+
+# Assignment
+OBJECTS = {}    # Runtime table of declared objects
+
+def exist(obj,val):
+    OBJECTS[obj] = val
+def has(obj,*args):
+    OBJECTS[obj] = args
+
+# Verb->Function lookup table
+VERBS = {
+    "add": [add,inc],
+    "increment": [inc],
+    "increase": [add,inc],
+
+    "sub": [sub,dec],
+    "subtract": [sub,dec],
+    "decrement": [dec],
+    "decrease": [sub,dec],
+
+    "multiply": [mul],
+
+    "divide": [div,mod],
+
+    "is": [exist],
+    "has": [has,exist]
 }
 
 
@@ -29,7 +109,7 @@ for sentence in text:
         if word.type == 'CD' and word.chunk is None:
             word.chunk = Chunk(sentence, words=[word], type='NP')
 # Print resulting structure
-pprint(parse(paragraph, relations=True, lemmata=True))
+#pprint(parse(paragraph, relations=True, lemmata=True))
 
 ### Step through each clause and map to function calls
 for sentence in text:
@@ -38,22 +118,19 @@ for sentence in text:
         subj = verb.subject or verb.previous(type='NP')
         obj = verb.object or verb.next(type='NP')
 
-        if subj: code += subj.head.string + '.'
-        code += verb.head.lemma + '('
-        if obj: code += obj.head.string
-        code += ')'
-        code += "\n"
+        # Generate pseudocode function call (Sprint 3)
+        if subj: pseudocode += subj.head.string + '.'
+        pseudocode += verb.head.lemma + '('
+        if obj: pseudocode += obj.head.string
+        pseudocode += ')'
+        pseudocode += "\n"
 
-print(code)
+        ### Compile actual function call
+        pass
+        # Lookup verb, ask user to choose function (or skip), ask user to include context variable
+        # Make real function call and set context variable to result
 
 
-VERBS = {
+print(pseudocode)
+print(OBJECTS)  # Dump objects created from this input
 
-}
-
-FUNCS = [
-    {
-        "desc": "assignment / existence.",
-        "func": None
-    }
-]
