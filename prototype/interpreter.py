8f55533d69c5b0463ac62b77a45cb82f37556a2c
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2019-05-07 04:22:50 -0500

    Sprint 3

--- prototype/interpreter.py
+++ prototype/interpreter.py
@@ -1,14 +1,9 @@
 import fileinput
-import nltk.chunk       # Chunking tools to translate groups of verbs/nouns into Subject-Verb-Object
-import nltk.chunk.util
-import nltk.chunk.regexp
-from nltk.stem import WordNetLemmatizer
-import nltk.tag         # Part-of-Speech Tagging (to determine verbs, nouns, etc)
-import nltk.tokenize    # Breaking down raw input into words / symbols
-
+from pattern.en import parse, pprint, parsetree, Chunk
+from pattern.search import search
 
 ### Init
-wordnet = WordNetLemmatizer()
+code = ""   # Container for pseudocode (Sprint 3 demonstration of function calls)
 
 # Declare contextual variables (inspired by Perl speclal/default variables)
 # Nouns are basically copied here when referenced so that pronouns / implicit references can access them
@@ -23,22 +18,33 @@ context_vars = {
 ### Here begins the REPL -- Read, Eval, Print Loop
 ### Our interpreter accepts either an input filename or raw input from STDIN.
 ### So far: For each sentence, our interpreter breaks it out into a sequence of actions based on the verbs identified.
-for paragraph in fileinput.input():
-    if paragraph.isspace(): continue    # Skip empty lines
+# Remove newlines by joining each line into a single string
+paragraph = " ".join(fileinput.input())
 
-    # Break down paragraph into sentences, then into words/tokens
-    sentences = [[word for word in nltk.word_tokenize(sentence)] for sentence in nltk.sent_tokenize(paragraph)]
-    for sentence in [nltk.tag.pos_tag(sentence) for sentence in sentences]:
-        print(sentence)
-        ### Get Subject-Verb-Object chunks
-        # Define regex rules for identifying SVO
-        chunk_rules = [
-            "<VB.?><CD>(?!<NN.?>)"  # Example: Get verbs followed by a number that *isn't* counting objects (e.g. "add seven")
-        ]
-        # Get chunked result
-        chunked_sentence = RegexpChunkParser([ChunkRule(cr) for cr in chunk_rules], chunk_label='SVO').parse(sentence)
-        # Step through each clause and map to function calls
-        pass
+### Get Subject-Verb-Object chunks
+text = parsetree(paragraph, relations=True, lemmata=True)
+# Treat all unchunked numbers ('CD's) as noun phrases
+for sentence in text:
+    for word in sentence:
+        if word.type == 'CD' and word.chunk is None:
+            word.chunk = Chunk(sentence, words=[word], type='NP')
+# Print resulting structure
+pprint(parse(paragraph, relations=True, lemmata=True))
+
+### Step through each clause and map to function calls
+for sentence in text:
+    for verb in sentence.verbs:
+        # Extract subject & object from parser, or assume nearest noun phrases
+        subj = verb.subject or verb.previous(type='NP')
+        obj = verb.object or verb.next(type='NP')
+
+        if subj: code += subj.head.string + '.'
+        code += verb.head.lemma + '('
+        if obj: code += obj.head.string
+        code += ')'
+        code += "\n"
+
+print(code)
 
 
 VERBS = {
