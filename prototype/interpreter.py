81e5cd944e517aef54b70d0a6d8627f14259ef8e
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2019-05-07 17:40:58 -0500

    Almost done but need to bang out the output

--- prototype/interpreter.py
+++ prototype/interpreter.py
@@ -1,8 +1,31 @@
+# Front-end imports
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output,Input,State
+# Back-end imports
 import fileinput
-from pattern.en import parse, pprint, parsetree, Chunk
+from pattern.en import parse, pprint, parsetree, Chunk, number
 from pattern.search import search
 
-### Init
+### Init front-end
+css = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=css)
+# Page layout
+app.layout([
+    # Input box
+    html.Div([
+        dcc.Textarea(placeholder='Paste input "program" here', id='input'),
+        html.Button('Compile', id='submit')
+    ]),
+    # Compilation options
+    html.Div((id="compile"),
+    # Output
+    html.Div([id="output")
+])
+
+
+### Init back-end
 pseudocode = ""   # Container for pseudocode (Sprint 3 demonstration of function calls)
 
 # Declare contextual variables (inspired by Perl speclal/default variables)
@@ -104,42 +127,114 @@ VERBS = {
 }
 
 
-### Here begins the REPL -- Read, Eval, Print Loop
-### Our interpreter accepts either an input filename or raw input from STDIN.
-### So far: For each sentence, our interpreter breaks it out into a sequence of actions based on the verbs identified.
-# Remove newlines by joining each line into a single string
-paragraph = " ".join(fileinput.input())
-
-### Get Subject-Verb-Object chunks
-text = parsetree(paragraph, relations=True, lemmata=True)
-# Treat all unchunked numbers ('CD's) as noun phrases
-for sentence in text:
-    for word in sentence:
-        if word.type == 'CD' and word.chunk is None:
-            word.chunk = Chunk(sentence, words=[word], type='NP')
-# Print resulting structure
-#pprint(parse(paragraph, relations=True, lemmata=True))
-
-### Step through each clause and map to function calls
-for sentence in text:
-    for verb in sentence.verbs:
-        # Extract subject & object from parser, or assume nearest noun phrases
-        subj = verb.subject or verb.previous(type='NP')
-        obj = verb.object or verb.next(type='NP')
-
-        # Generate pseudocode function call (Sprint 3)
-        if subj: pseudocode += subj.head.string + '.'
-        pseudocode += verb.head.lemma + '('
-        if obj: pseudocode += obj.head.string
-        pseudocode += ')'
-        pseudocode += "\n"
-
-        ### Compile actual function call
-        pass
-        # Lookup verb, ask user to choose function (or skip), ask user to include context variable
-        # Make real function call and set context variable to result
+### Front-end routines
+# Serve initial input form
 
 
-print(pseudocode)
-print(OBJECTS)  # Dump objects created from this input
+# Query user for compilation
+def query_user():
+    pass
+
+# Output
+
+
+
+
+
+#print(pseudocode)
+#print(OBJECTS)  # Dump objects created from this input
+
+
+### Dash I/O
+# Compile input
+@app.callback(
+    Output('compile', 'children'),
+    [Input('submit', 'n_clicks')],
+    [State('input', 'value')]
+)
+def compile_input(n_clicks, text_input):
+    # For each sentence, our interpreter breaks it out into a sequence of actions based on the verbs identified.
+    # Remove newlines by joining each line into a single string
+    paragraph = " ".join(text_input)
+
+    ### Get Subject-Verb-Object chunks
+    text = parsetree(paragraph, relations=True, lemmata=True)
+    # Treat all unchunked numbers ('CD's) as noun phrases
+    for sentence in text:
+        for word in sentence:
+            if word.type == 'CD' and word.chunk is None:
+                word.chunk = Chunk(sentence, words=[word], type='NP')
+    # Print resulting structure
+    #pprint(parse(paragraph, relations=True, lemmata=True))
+
+    html_form = []  # Container for HTML form children
+    ### Step through each clause and map to function calls
+    for sentence in text:
+        for verb in sentence.verbs:
+            # Extract subject & object from parser, or assume nearest noun phrases
+            subj = verb.subject or verb.previous(type='NP')
+            obj = verb.object or verb.next(type='NP')
+
+            # Generate pseudocode function call (Sprint 3)
+            if subj: pseudocode += subj.head.string + '.'
+            pseudocode += verb.head.lemma + '('
+            if obj: pseudocode += obj.head.string
+            pseudocode += ')'
+            pseudocode += "\n"
+
+            ### Compile actual function call
+            # Lookup verb
+            opts = [ {'label': 'Ignore this Verb Phrase', 'value': -1} ]
+            if verb.head.lemma in VERBS:
+                opts.extend( [{'label': func.__name__, 'value': i} for i,func in enumerate(VERBS[verb.head.lemma])] )
+
+            # Ask user to choose interpretation of Verb Phrase
+            html_form.extend([
+                html.H3(subj + " " + verb + " " + obj),
+                dcc.Dropdown(options=opts, placeholder="Select interpretation"),
+                dcc.Checklist(options=[{'label': 'Also pass context?', 'value'='con_var'}])
+            ])
+    html_form.append(html.Button('Process', id='process'))
+    return html_form
+
+# Output
+@app.callback(
+    Output('output', 'children'),
+    [Input('process', 'n_clicks')],
+    [State('input', 'value'), State('compile', 'children')]
+)
+def output(n_clicks, text_input, options):
+    # Remove newlines by joining each line into a single string
+    paragraph = " ".join(text_input)
+
+    ### Get Subject-Verb-Object chunks
+    text = parsetree(paragraph, relations=True, lemmata=True)
+    # Treat all unchunked numbers ('CD's) as noun phrases
+    for sentence in text:
+        for word in sentence:
+            if word.type == 'CD' and word.chunk is None:
+                word.chunk = Chunk(sentence, words=[word], type='NP')
+    # Print resulting structure
+    #pprint(parse(paragraph, relations=True, lemmata=True))
+
+    html_out = []  # Container for HTML output div children
+    o = 0          # Option index (index of div 'compile' children)
+    ### Step through each clause and map to function calls
+    for sentence in text:
+        for verb in sentence.verbs:
+            # Extract subject & object from parser, or assume nearest noun phrases
+            subj = verb.subject or verb.previous(type='NP')
+            obj = verb.object or verb.next(type='NP')
+
+            # Compute function call
+            args = []
+            if subj: args.append(subj)
+            o+=1    # Get interpretation dropdown
+            if options[o].value >= 0
+                VERBS[verb.head.lemma][options[o].value](
+            o+=1    # Get context var checkbox
+
+            # Generate output
+
+
 
